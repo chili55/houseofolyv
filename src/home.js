192d51f0009ff8642182741613dc17d4054d397d
Mika <miiikhaella@gmail.com>
2025-10-19 22:51:58 +0800

    Initial commit

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css"; // Ensure Tailwind CSS is imported
-import logo from "./assets/OLYV LOGO.png";
+
 import p1 from "./assets/p1.jpg";
 import p2 from "./assets/p2 .jpg";
 import p3 from "./assets/p3.jpeg";
